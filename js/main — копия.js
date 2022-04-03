c8f822cc47c3828e1db80885a72266cdaef055d4
mk3mk <mk3mk@yandex.ru>
2022-04-03 21:07:36 +0300

    03-04-2022

--- js/main — копия.js
+++ js/main — копия.js
@@ -49,6 +49,7 @@ infoDelete.innerHTML = 'удалить = ' + btnDeleteAll.length;
 
 for ( let i = 0; i < timerContainer.length; i++ ){
   timerNumber[i].innerHTML = i;
+  timerNumber[i].style.background = 'green';
 }
 
 
@@ -122,77 +123,6 @@ function plusTimer(){
   newTimer.querySelector('.btn-delete').style.background = "red";
   newTimer.querySelector('.btn-delete').addEventListener('click', deleteThisTimer);
   newTimer.querySelector('.btn-add').addEventListener('click', plusTimer);
-  newTimer.querySelector('.btn-start').addEventListener('click', startThisTimer);
-
-  btnStart  = document.querySelectorAll('.btn-start');
-
-  function startThisTimer(){
-    this.closest('.timer-container').style.background = 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)';
-    this.closest('.timer-container').querySelector('.fcs').style.background = 'green';
-
-      let fcs = this.closest('.timer-container').querySelector('.fcs');
-      let fcm = this.closest('.timer-container').querySelector('.fcm');
-      let fch = this.closest('.timer-container').querySelector('.fch');
-      let fcd = this.closest('.timer-container').querySelector('.fcd');
-      let tmok = this.closest('.timer-container').querySelector('.timer-end');
-      let btnClose = this.closest('.timer-container').querySelector('.btn-close');
-      let btnDelete = this.closest('.timer-container').querySelector('.btn-delete');
-      let btnAdd = this.closest('.timer-container').querySelector('.btn-add');
-
-      
-      let btnPause = this.closest('.timer-container').querySelector('.btn-pause');
-      let btnStop = this.closest('.timer-container').querySelector('.btn-stop');
-
-      this.classList.add('btn-invisible');
-      btnPause.classList.remove('btn-invisible');
-      btnStop.classList.remove('btn-invisible');
-      
-
-      fcs.style.background = 'green';
-      fcm.style.background = 'cyan';
-      fch.style.background = 'gold';
-      fcd.style.background = 'red';
-
-      let fcs_value = fcs.value;
-      let fcm_value = fcm.value;
-      let fch_value = fch.value;
-      let fcd_value = fcd.value;
-
-      console.log('клик по кнопке Запуск, сейчас таких кнопок =' + btnStart.length)
-            
-      func_fcs();
-      function func_fcs(){
-
-      timerMain = setTimeout(function(){
-            fcs.value = fcs_value;
-            fcm.value = fcm_value;
-            fch.value = fch_value;
-            fcd.value = fcd_value;
-              if( fcs_value > 0){
-                fcs_value --;
-              }else if( fcs_value == 0 && fcm_value > 0){
-                fcs_value = 59;
-                fcm.value --;
-                fcm_value --;
-              }else if( fcs_value == 0 && fcm_value == 0 && fch_value > 0){
-                fcs_value = 59;
-                fcm_value = 59;
-                fch.value --;
-                fch_value --;
-              }else if( fcs_value == 0 && fcm_value == 0 && fch_value == 0 && fcd_value > 0 ){
-                fcs_value = 59;
-                fcm_value = 59;
-                fch_value = 24;
-                fcd.value --;
-                fcd_value --;
-              }else if( fcs_value == 0 && fcm_value == 0 && fch_value == 0 && fcd_value == 0 && port == false ){
-                func_timer_stop();
-              }
-            func_fcs();
-        }, 30);  
-      }
-
-  }
 
   function deleteThisTimer(){
 
@@ -241,10 +171,10 @@ for (let i = 0; i < btnStart.length; i++) {
       let btnAdd = this.closest('.timer-container').querySelector('.btn-add');
 
       
-      let btnPause = this.closest('.timer-container').querySelector('.btn-pause');
-      let btnStop = this.closest('.timer-container').querySelector('.btn-stop');
+      let btnPause = btnStart[i].closest('.timer-container').querySelector('.btn-pause');
+      let btnStop = btnStart[i].closest('.timer-container').querySelector('.btn-stop');
 
-      this.classList.add('btn-invisible');
+      btnStart[i].classList.add('btn-invisible');
       btnPause.classList.remove('btn-invisible');
       btnStop.classList.remove('btn-invisible');
       
@@ -306,16 +236,16 @@ for (let i = 0; i < btnStart.length; i++) {
 
   function func_timer_stop(){
 
-    if (i == 1){
+    if (i == 0){
       btnStart[i].style.background = 'red';
       btnStart[i].closest('.timer-container').querySelector('.timer-end').style.background = 'url("img/bg-01.gif") center center/100% 100% no-repeat';
-    }else if (i == 2){
+    }else if (i == 1){
       btnStart[i].style.background = 'green';
       btnStart[i].closest('.timer-container').querySelector('.timer-end').style.background = 'url("img/bg-02.gif") center center/100% 100% no-repeat';
-    }else if (i == 3){
+    }else if (i == 2){
       btnStart[i].style.background = 'blue';
       btnStart[i].closest('.timer-container').querySelector('.timer-end').style.background = 'url("img/bg-03.gif") center center/100% 100% no-repeat';
-    }else if (i == 4 || i > 4){
+    }else if (i == 3 || i > 3){
       btnStart[i].style.background = 'cyan';
       btnStart[i].closest('.timer-container').querySelector('.timer-end').style.background = 'red';
     }
